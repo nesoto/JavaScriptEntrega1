fd6b576eec4914089b33a03fbfa550d960cfd5a0
Nesoto <nsoto2020@inf.udec.cl>
2021-10-20 00:23:40 -0300

    redi

---
+++ js/entrega1.js
@@ -0,0 +1,19 @@
+
+let var1;
+let var2;
+
+let nombre = prompt("¿Como te llamas?");
+alert("Hola " + nombre);
+let RESULTADO;
+
+let respuesta = prompt("¿Me darías dos numeros para sumarlos?");
+
+if (respuesta == "Si" || respuesta == "si"){
+	var1 = Number(prompt("Primer número"));
+	var2 = Number(prompt("Segundo número"));
+	RESULTADO = var1 + var2;
+	alert("Gracias! el resultado es " + RESULTADO);
+}else if( respuesta == "No" || respuesta == "no"){
+	RESULTADO = 0;
+	alert("Esta bien :C");
+}
